4414918c6f8b1cb5318c5c7015a2a3c9166ddd24
Prakash9019 <plsprakash2003@gmail.com>
2025-01-24 12:33:46 +0530

    added

--- index.js
+++ index.js
@@ -21,59 +21,7 @@ app.use(express.json());
 
 app.use('/api/auth',require('./routers/auth.js'));
 app.use('/api/notes',require('./routers/notes'));
-// app.use('/api/qrcode',require('./routers/qrcode'));
 
-// const UserSchema = new mongoose.Schema({
-//     username: String,
-//     schoolName: String,
-//     qrCode: String, // Store the QR code image URL
-//   });
-  
-//   const UserModel = mongoose.model('User', UserSchema);
-  
-//   app.use(bodyParser.json());
-  
-
-//   app.post('/api/generate-qr', async (req, res) => {
-//     try {
-//       const { username, schoolName } = req.body;
-  
-//       // Generate QR code
-//       const qrCodeData = `"Username is"+${username} - ${schoolName}`;
-//       const qrCodeImage = await qrcode.toDataURL(qrCodeData);
-  
-//       // Save user data and QR code URL to MongoDB
-//       const newUser = new UserModel({
-//         username:username,
-//         schoolName: schoolName,
-//         qrCode: qrCodeImage,
-//       });
-//       await newUser.save();
-  
-//       res.status(200).json({ qrCodeImage });
-//     } catch (error) {
-//       console.error('Error generating QR code:', error);
-//       res.status(500).json({ error: 'Internal server error' });
-//     }
-//   });
-
-//   app.get('/api/scan-qr/:id', async (req, res) => {
-//     try {
-        
-//       const userId = req.params.userId;
-//     //   console.log( req.params);
-//     // userId
-//       // Look up the user profile based on userId
-//       const userProfile = await UserModel.findById(userId);
-//       if (!userProfile) {
-//         return res.status(404).json({ error: 'User profile not found' });
-//       }
-//       res.status(200).json(userProfile);
-//     } catch (error) {
-//       console.error('Error fetching user profile:', error);
-//       res.status(500).json({ error: 'Internal server error' });
-//     }
-//   });
   
 app.get('/',(req,res)=>{
     res.json("hello");
@@ -81,5 +29,5 @@ app.get('/',(req,res)=>{
 
 
 app.listen(PORT, () => {
-    console.log(`Todolist listening at http://localhost:${PORT}`);
+    console.log(`Vertxai listening at http://localhost:${PORT}`);
 })
