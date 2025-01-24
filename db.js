5ec112fc24111c7dde0aa885950655977d2ee855
Pentakotacharan <charanpentakota2006@gmail.com>
2025-01-24 22:35:44 +0530

    added

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const exp=require('express');
 const mongoose=require('mongoose');
 mongoose.set('strictQuery',true);
+
+require('dotenv').config();
 // mongodb+srv://Prakash:<password>@cluster0.emqxvew.mongodb.net/?retryWrites=true&w=majority
+
+const hii=process.env.MONGO_URL;
 const connectDB= ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/vertxai3"
+    mongoose.connect(`mongodb+srv://plsprakash2003:${hii}@cluster0.uygol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("connected");
 }
 
 
 // mongodb+srv://plsprakash2003:Surya_2003@cluster0.2yh1df7.mongodb.net/pro?retryWrites=true&w=majority&ssl=true
+// mongodb+srv://plsprakash2003:<db_password>@cluster0.uygol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 module.exports=connectDB;
 //mongodb://localhost:27017
