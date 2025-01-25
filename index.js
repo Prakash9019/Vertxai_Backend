a7cfcf5e98f5476e977aea0eae745c8fdb40edea
Prakash9019 <plsprakash2003@gmail.com>
2025-01-25 15:22:33 +0530

    add

--- index.js
+++ index.js
@@ -9,27 +9,10 @@ const app = express();
 const PORT=5000;
 connectDB();
 
-app.use(cors({
-    origin: ["http://localhost:5173", "https://your-frontend-production.com"], // Allow these origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
-    credentials: true, // Allow cookies and authorization headers
-}));
-
-app.options("*", cors()); // Handle preflight requests
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Allow specific origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow headers
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204); // Respond to preflight request
-    }// lkmkmk
-    next();
-});
 
 
-// app.use(cors());
+
+app.use(cors());
 app.use(express.json());
 
 
