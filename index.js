eace35194ac0b8a57c818fda0acd1cecc8d71789
Prakash9019 <plsprakash2003@gmail.com>
2025-01-25 11:31:07 +0530

    added

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const connectDB = require('./db');
 const express = require('express');
-
+var cors=require('cors');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const qrcode = require('qrcode');
+//connectDB();
 const app = express();
-const PORT = 5000;
-
-// Connect to the database
+const PORT=5000;
 connectDB();
 
-// Middleware to parse JSON
+// app.use(cors({
+//     origin: [
+//         "https://vertxai-backend.vercel.app", // Production frontend
+//         "http://localhost:5173" // Development frontend
+//     ],
+//     methods: ["POST", "GET", "OPTIONS"],
+//     credentials: true // Allow cookies/credentials
+// }));
+
+// app.options("*", cors()); // Handle preflight requests
+// app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', require('./routers/auth.js'));
-app.use('/api/notes', require('./routers/notes'));
 
-// Test route
-app.get('/', (req, res) => {
-    res.json("Hello, this is VertxAI!");
-});
+app.use('/api/auth',require('./routers/auth.js'));
+app.use('/api/notes',require('./routers/notes'));
 
-// Start the server
+  
+app.get('/',(req,res)=>{
+    res.json("hello");
+})
+
+
 app.listen(PORT, () => {
-    console.log(`VertxAI listening at http://localhost:${PORT}`);
-});
+    console.log(`Vertxai listening at http://localhost:${PORT}`);
+})  
