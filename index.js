c7080d6f8e14d8bb125329a2084f7a34049bc9d1
Prakash9019 <plsprakash2003@gmail.com>
2025-01-25 15:18:25 +0530

    added

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204); // Respond to preflight request
-    }
+    }// lkmkmk
     next();
 });
 
