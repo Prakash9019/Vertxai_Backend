30a5ef44d1703845db0d30fca533f732d229a5a0
Prakash9019 <plsprakash2003@gmail.com>
2025-02-06 07:07:13 +0530

    added

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS Configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: 'https://vertxai.vercel.app/' || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
